d58e53a3efc923f06b4e8d577eb98dee9aa8ff96
Mike Starbuck <miglas9@gmail.com>
2019-05-15 23:53:22 -0500

    tile and movement

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,9 +1,9 @@
 export const SPRITE_SIZE = 40 // size of player sprite for move calculations
 
 // size of MAP for player boundaries in movement
-export const MAP_HEIGHT = SPRITE_SIZE * 10
+export const MAP_HEIGHT = 80 * 10
 
-export const MAP_WIDTH = SPRITE_SIZE * 20
+export const MAP_WIDTH = 80 * 20
 
 // export const NEW_WIDTH = 80
 
